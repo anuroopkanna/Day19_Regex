d78f8914441c70970a52943902c6cba6403fdc0f
anuroopkanna <anuroopindia16@gmail.com>
2023-03-31 21:07:17 +0530

    added:userRegistration

---
+++ UserRegistration.java
@@ -0,0 +1,10 @@
+package com.BridgeLabzDay19;
+
+import java.util.Scanner;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("***** Welcome to User Registration *****");
+    }
+}
