319cc799429eab2f5ad17b6ada9d4e8e08de2236
anuroopkanna <anuroopindia16@gmail.com>
2023-04-02 00:27:18 +0530

    added:uc2

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,10 +1,23 @@
 package com.BridgeLabzDay19;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("***** Welcome to User Registration *****");
+        System.out.print("Enter Fist name:- ");
+        String firstName = sc.next();
+        System.out.print("Enter Last name:- ");
+        String lastName = sc.next();
+        Pattern pattern = Pattern.compile("^[A-Z]?[a-z]{3,}$");
+        Matcher matcher = pattern.matcher(firstName);
+        Matcher matcher1 = pattern.matcher(lastName);
+        if (matcher.matches()&& matcher1.matches())
+            System.out.println("valid");
+        else
+            System.out.println("Invalid");
     }
 }
